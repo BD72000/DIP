71f971c6d8fce925d3ed849823faed26a5ad8248
BD72000 <140050070+BD72000@users.noreply.github.com>
2025-08-04 23:53:03 +0530

    Lab - 1

---
+++ img2bw.m
@@ -0,0 +1,33 @@
+img = imread("C:\Users\bhavy\Downloads\img.jpg");  
+
+[rows, cols, channels] = size(img);
+
+gray_img = zeros(rows, cols, 'uint8');
+
+for i = 1:rows
+    for j = 1:cols
+        R = double(img(i,j,1));
+        G = double(img(i,j,2));
+        B = double(img(i,j,3));
+
+        gray_val = (R + G + B) / 3;
+         gray_img(i,j) = uint8(gray_val);
+    end
+end
+bw_img = zeros(rows, cols, 'uint8'); 
+threshold = 128;
+for i = 1:rows
+    for j = 1:cols
+        if gray_img(i,j) >= threshold
+            bw_img(i,j) = 255; 
+        else
+            bw_img(i,j) = 0; 
+        end
+    end
+end
+
+figure;
+subplot(1,2,1); imshow(img); 
+title('Original RGB Image');
+subplot(1,2,2); imshow(bw_img); 
+title('BW Image(Threshold=128)');
