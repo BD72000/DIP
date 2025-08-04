8ba5a6b2b7a96d4b963587e6b40d969d9b2aa204
BD72000 <140050070+BD72000@users.noreply.github.com>
2025-08-04 23:42:17 +0530

    Lab - 1

---
+++ img2gray.m
@@ -0,0 +1,21 @@
+img = imread("C:\Users\bhavy\Downloads\img.jpg");  
+
+[rows, cols, channels] = size(img);
+
+gray_img = zeros(rows, cols, 'uint8');
+
+for i = 1:rows
+    for j = 1:cols
+        R = double(img(i,j,1));
+        G = double(img(i,j,2));
+        B = double(img(i,j,3));
+
+        gray_val = (R + G + B) / 3;
+         gray_img(i,j) = uint8(gray_val);
+    end
+end
+figure;
+subplot(1,2,1); imshow(img); 
+title('Original RGB Image');
+subplot(1,2,2); imshow(gray_img); 
+title('Grayscale Image (RGB Average)');
---
+++ img_2_Only_R_G_B.m
@@ -0,0 +1,18 @@
+img = imread("C:\Users\bhavy\Downloads\img.jpg");  
+
+[rows, cols, channels] = size(img);
+
+red_img = zeros(rows, cols, 3, 'uint8');
+red_img(:,:,1) = img(:,:,1);
+
+green_img = zeros(rows, cols, 3, 'uint8');
+green_img(:,:,2) = img(:,:,2); 
+
+blue_img = zeros(rows, cols, 3, 'uint8');
+blue_img(:,:,3) = img(:,:,3);
+
+figure;
+subplot(2,2,1); imshow(img); title('Original RGB Image');
+subplot(2,2,2); imshow(red_img); title('Red Only');
+subplot(2,2,3); imshow(green_img); title('Green Only');
+subplot(2,2,4); imshow(blue_img); title('Blue Only');
